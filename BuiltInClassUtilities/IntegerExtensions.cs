4459e186b6c1d7221b6325160997074d50b486cb
LightyTheLight <Break_Ender233@163.com>
2023-04-24 05:31:06 +0800

    添加循环取余函数

---
+++ BuiltInClassUtilities/IntegerExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TisGameKit.TisGameKitCommon.BuiltInClassUtilities
+{
+    public static class IntegerExtensions
+    {
+        /// <summary>
+        /// 循环取余，对于负数的处理如下
+        /// -5 % 3 = 1
+        /// </summary>
+        public static uint LoopModular(this int value, uint modular)
+        {
+            if (value >= 0)
+            {
+                return (uint)value % modular;
+            }
+            value = -value;
+            var result = value % modular;
+            return ((uint)(modular - result));
+        }
+    }
+}
